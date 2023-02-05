c8ccbf198ed625f580a68f50849fc5a35b509c1e
catwizard1 <124324348+catwizard1@users.noreply.github.com>
2023-02-04 23:24:36 -0600

    Update api.lua

--- bin/classes/api.lua
+++ bin/classes/api.lua
@@ -1,23 +1,27 @@
 local api = {}
 local endp = require('../attribs/endpoints')
 local http = require('coro-http')
+local json = require('json')
 local base = 'https://discord.com/api/v10'
 local format = string.format
+local insert = table.insert
 
 api.__index = api
 
 function api:new(token)
-    return setmetatable({_token = token}, api)
+    return setmetatable({
+        _token = token
+    }, api)
 end
 
-function api:send(method, endpoint)
-    return http.request(method, format('%s/%s', base, endpoint), {
-        {'Authorization', format('Bot %s', self._token)}
-    })
+function api:send(method, endpoint, headers, body)
+    insert(headers, {'Authorization', format('Bot %s', self._token)})
+
+    return http.request(method, format('%s/%s', base, endpoint), headers, body)
 end
 
 function api:getbotgateway()
-    return http.request('GET', endp.gateway_bot)
+    return self:send('GET', endp.gateway_bot, {})
 end
 
 return api
