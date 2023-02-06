90a665bf790081821dbe1dfcf67ff0869d8e1405
catwizard1 <124324348+catwizard1@users.noreply.github.com>
2023-02-05 22:36:00 -0600

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,4 +1,4 @@
 local class = require('./bin/classes/client')
-local client = class:new({ token = 'MTA3MDgzMTE3MzU3Nzc0MDMxOA.G9m9NT.ivjv5StkmKHlNrdZrfBSN5VAXD0lDQGwi8lY2g' })
+local client = class:new({ token = '' })
 
 client:login()
